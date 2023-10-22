86622783b9585c567a0a02747cc78170eadd15d4
pajserica <pajaa.2002@gmail.com>
2023-10-23 00:03:15 +0200

    asd
    asd

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+}
