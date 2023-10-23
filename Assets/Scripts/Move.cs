1e5cb9d8192188b08f752134e90048e5ee7a83a4
pajserica <pajaa.2002@gmail.com>
2023-10-23 21:05:10 +0200

    Move func
    Menjanje perspektive na 123 keys

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -22,6 +22,13 @@ public class Move : MonoBehaviour
     [SerializeField] float crouchYScale;
      float startYScale;
  
+    [Header("CameraSwitch")]
+    public Transform combatLookAt;
+    [SerializeField] GameObject basicCam;
+    [SerializeField] GameObject topDownCam;
+    [SerializeField] GameObject combatCam;
+
+
     [Header("Ground Check")]
     [SerializeField] float playerHeight;
     [SerializeField] LayerMask Ground;
@@ -32,9 +39,13 @@ public class Move : MonoBehaviour
     public KeyCode jumpKey = KeyCode.Space;
     public KeyCode sprintKey = KeyCode.LeftShift;
     public KeyCode crouchKey = KeyCode.LeftControl;
+    public KeyCode BasicCamKey = KeyCode.Alpha1;
+    public KeyCode TopDownCamKey = KeyCode.Alpha2;
+    public KeyCode CombatCamKey = KeyCode.Alpha3;
+
 
     [Header("References")]
-    public Transform cam;
+    [SerializeField] GameObject mainCamObj;
     public Transform playerObj;
     [SerializeField] Transform orientation;
 
@@ -51,6 +62,13 @@ public class Move : MonoBehaviour
         sprinting,
         crouching,
         air
+    }
+
+    public CameraState camState;
+    public enum CameraState{
+        Basic,
+        Combat,
+        Topdown
     }
 
     // Start is called before the first frame update
@@ -88,6 +106,10 @@ public class Move : MonoBehaviour
         if(Input.GetKeyUp(crouchKey)){
             transform.localScale = new Vector3(transform.localScale.x, startYScale, transform.localScale.z);
         }
+        if(Input.GetKeyDown(BasicCamKey)) SwitchCameraView(CameraState.Basic);
+        if(Input.GetKeyDown(TopDownCamKey)) SwitchCameraView(CameraState.Topdown);
+        if(Input.GetKeyDown(CombatCamKey)) SwitchCameraView(CameraState.Combat);
+
     }
 
     private void MoveStateHandler(){
@@ -108,9 +130,18 @@ public class Move : MonoBehaviour
     }
 
     private void RotatePlayer(){
-        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        if(inputDir != Vector3.zero){
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, rotationSpeed * Time.deltaTime);
+        //easy rotate if these two cams
+        if(camState == CameraState.Basic || camState == CameraState.Topdown){
+            Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+            if(inputDir != Vector3.zero)
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, rotationSpeed * Time.deltaTime);
+        }
+        //hard rotation for combat
+        else if(camState == CameraState.Combat){
+            Vector3 combatViewDir = combatLookAt.position - new Vector3(mainCamObj.transform.position.x, combatLookAt.position.y, mainCamObj.transform.position.z);
+            orientation.forward = combatViewDir.normalized;
+
+            playerObj.forward = combatViewDir.normalized;
         }
     }
 
@@ -139,6 +170,26 @@ public class Move : MonoBehaviour
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
+    private void SwitchCameraView(CameraState newView){
+        basicCam.SetActive(false);
+        topDownCam.SetActive(false);
+        combatCam.SetActive(false);
+
+        if(newView == CameraState.Basic){
+            basicCam.SetActive(true);
+            mainCamObj = basicCam;
+        }if(newView == CameraState.Topdown){
+            topDownCam.SetActive(true);
+            mainCamObj = topDownCam;
+        }if(newView == CameraState.Combat){
+            combatCam.SetActive(true);
+            mainCamObj = combatCam;
+        } 
+
+        camState = newView;
+         
+    }
+
     private void ResetJump(){
         readyToJump = true;
     }
@@ -149,7 +200,7 @@ public class Move : MonoBehaviour
         // ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + extendedGrCheck, Ground);
         //view direction (orientation)
-        Vector3 viewDir = transform.position - new Vector3(cam.position.x, transform.position.y, cam.position.z);
+        Vector3 viewDir = transform.position - new Vector3(mainCamObj.transform.position.x, transform.position.y, mainCamObj.transform.position.z);
         orientation.forward = viewDir.normalized;
 
         MyInput();
